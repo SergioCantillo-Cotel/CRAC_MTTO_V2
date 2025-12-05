e13210d39df489d55e8a87c65fdceddda426c431
SergioCantillo-Cotel <sergio.cantillo@cotel.com.co>
2025-12-04 20:17:38 -0500

    Delete crm-sync-api directory

--- crm-sync-api/app/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-CRM Sync API - Sincronización de datos del CRM a PostgreSQL
-Autor: Cotel Investments I+D+i Team
-Version: 1.0.0
-"""
-
-__version__ = "1.0.0"
-__author__ = "Cotel Investments I+D+i Team"
